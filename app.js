6374d2590bf2ff0b0800f170b979cf113d242f1d
C M Nafi <naficm01@luther.edu>
2020-12-10 19:58:50 -0600

    update folders

--- app.js
+++
@@ -1,8 +0,0 @@
-// jshint esversion :8
-// jshint browser: true
-
-console.log("Hello")
-function paint (the_color) {
-    let header = document.querySelector("h1");
-    header.setAttribute("style",`color: ${the_color}`);
-}
