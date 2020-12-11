04c562d70794e37f9066bb68f1f88822b8350ad9
C M Nafi <naficm01@luther.edu>
2020-12-10 19:49:08 -0600

    pre-update

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const express = require('express');
+// jshint esversion :8
+// jshint browser: true
+
+console.log("Hello")
+function paint (the_color) {
+    let header = document.querySelector("h1");
+    header.setAttribute("style",`color: ${the_color}`);
+}
