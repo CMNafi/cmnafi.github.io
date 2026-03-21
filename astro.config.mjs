0494cf9c7c9d11c996f3c1f1ad3fc4d7406080d4
CMNafi <naficm01@gmail.com>
2026-03-20 22:38:20 -0400

    fix: configure astro sitemap generator and robots.txt domains

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cmnafi.com',
-  integrations: [mdx()],
+  site: 'https://www.cmnafi.com',
+  integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: 'shiki'
   }
